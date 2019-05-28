95afc63e80e780ddc39ec3326580845fb17a2169
gis300 <gisaw300@gmail.com>
2019-05-28 17:57:50 -0300

    login admin

--- vendor/hcodebr/php-classes/src/Model/User.php
+++ vendor/hcodebr/php-classes/src/Model/User.php
@@ -5,6 +5,8 @@ use \Hcode\DB\Sql;
 use \Hcode\Model;
 
 class User extends Model {
+
+	const SESSION = "User";
 	
 	public static function login($login, $password){
 
@@ -24,7 +26,11 @@ class User extends Model {
 
 			$user = new User();
 
-			$user->setiduser($data["iduser"]);
+			$user->setData($data);
+
+			$_SESSION[User::SESSION] = $user->getValues();
+
+			return $user;
 
 
 
@@ -32,6 +38,36 @@ class User extends Model {
 			throw new \Exception("Usuário inexistente ou senha inválida.");
 		}
 	}
+
+
+	public static function verifyLogin($inadmin = true)
+	{
+
+		if(
+			!isset($_SESSION[User::SESSION])
+			||
+			!$_SESSION[User::SESSION]
+			||
+			!(int)$_SESSION[User::SESSION]["iduser"] > 0
+			||
+			(bool)$_SESSION[User::SESSION]["inadmin"] !== $inadmin
+		)  {
+
+			header("Location: /admin/login");
+			exit;
+		}
+
+
+
+	}
+
+	public static function logout()
+	{
+
+		$_SESSION[User::SESSION] = NULL;
+
+	}
+
 }
 
 
