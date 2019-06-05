511609db4807e589c88ecc3ca1bfe9b34e680997
gis300 <gisaw300@gmail.com>
2019-06-05 16:32:15 -0300

    carrinho frete

--- vendor/hcodebr/php-classes/src/Model/Cart.php
+++ vendor/hcodebr/php-classes/src/Model/Cart.php
@@ -113,160 +113,160 @@ class Cart extends Model {
 
 	}
 
-	// public function addProduct(Product $product)
-	// {
+	public function addProduct(Product $product)
+	{
 
-	// 	$sql = new Sql();
+		$sql = new Sql();
 
-	// 	$sql->query("INSERT INTO tb_cartsproducts (idcart, idproduct) VALUES(:idcart, :idproduct)", [
-	// 		':idcart'=>$this->getidcart(),
-	// 		':idproduct'=>$product->getidproduct()
-	// 	]);
+		$sql->query("INSERT INTO tb_cartsproducts (idcart, idproduct) VALUES(:idcart, :idproduct)", [
+			':idcart'=>$this->getidcart(),
+			':idproduct'=>$product->getidproduct()
+		]);
 
-	// 	$this->getCalculateTotal();
+		$this->getCalculateTotal();
 
-	// }
+	}
 
-	// public function removeProduct(Product $product, $all = false)
-	// {
+	public function removeProduct(Product $product, $all = false)
+	{
 
-	// 	$sql = new Sql();
+		$sql = new Sql();
 
-	// 	if ($all) {
+		if ($all) {
 
-	// 		$sql->query("UPDATE tb_cartsproducts SET dtremoved = NOW() WHERE idcart = :idcart AND idproduct = :idproduct AND dtremoved IS NULL", [
-	// 			':idcart'=>$this->getidcart(),
-	// 			':idproduct'=>$product->getidproduct()
-	// 		]);
+			$sql->query("UPDATE tb_cartsproducts SET dtremoved = NOW() WHERE idcart = :idcart AND idproduct = :idproduct AND dtremoved IS NULL", [
+				':idcart'=>$this->getidcart(),
+				':idproduct'=>$product->getidproduct()
+			]);
 
-	// 	} else {
+		} else {
 
-	// 		$sql->query("UPDATE tb_cartsproducts SET dtremoved = NOW() WHERE idcart = :idcart AND idproduct = :idproduct AND dtremoved IS NULL LIMIT 1", [
-	// 			':idcart'=>$this->getidcart(),
-	// 			':idproduct'=>$product->getidproduct()
-	// 		]);
+			$sql->query("UPDATE tb_cartsproducts SET dtremoved = NOW() WHERE idcart = :idcart AND idproduct = :idproduct AND dtremoved IS NULL LIMIT 1", [
+				':idcart'=>$this->getidcart(),
+				':idproduct'=>$product->getidproduct()
+			]);
 
-	// 	}
+		}
 
-	// 	$this->getCalculateTotal();
+		$this->getCalculateTotal();
 
-	// }
+	}
 
-	// public function getProducts()
-	// {
+	public function getProducts()
+	{
 
-	// 	$sql = new Sql();
+		$sql = new Sql();
 
-	// 	$rows = $sql->select("
-	// 		SELECT b.idproduct, b.desproduct , b.vlprice, b.vlwidth, b.vlheight, b.vllength, b.vlweight, b.desurl, COUNT(*) AS nrqtd, SUM(b.vlprice) AS vltotal 
-	// 		FROM tb_cartsproducts a 
-	// 		INNER JOIN tb_products b ON a.idproduct = b.idproduct 
-	// 		WHERE a.idcart = :idcart AND a.dtremoved IS NULL 
-	// 		GROUP BY b.idproduct, b.desproduct , b.vlprice, b.vlwidth, b.vlheight, b.vllength, b.vlweight, b.desurl 
-	// 		ORDER BY b.desproduct
-	// 	", [
-	// 		':idcart'=>$this->getidcart()
-	// 	]);
+		$rows = $sql->select("
+			SELECT b.idproduct, b.desproduct , b.vlprice, b.vlwidth, b.vlheight, b.vllength, b.vlweight, b.desurl, COUNT(*) AS nrqtd, SUM(b.vlprice) AS vltotal 
+			FROM tb_cartsproducts a 
+			INNER JOIN tb_products b ON a.idproduct = b.idproduct 
+			WHERE a.idcart = :idcart AND a.dtremoved IS NULL 
+			GROUP BY b.idproduct, b.desproduct , b.vlprice, b.vlwidth, b.vlheight, b.vllength, b.vlweight, b.desurl 
+			ORDER BY b.desproduct
+		", [
+			':idcart'=>$this->getidcart()
+		]);
 
-	// 	return Product::checkList($rows);
+		return Product::checkList($rows);
 
-	// }
+	}
 
-	// public function getProductsTotals()
-	// {
+	public function getProductsTotals()
+	{
 
-	// 	$sql = new Sql();
+		$sql = new Sql();
 
-	// 	$results = $sql->select("
-	// 		SELECT SUM(vlprice) AS vlprice, SUM(vlwidth) AS vlwidth, SUM(vlheight) AS vlheight, SUM(vllength) AS vllength, SUM(vlweight) AS vlweight, COUNT(*) AS nrqtd
-	// 		FROM tb_products a
-	// 		INNER JOIN tb_cartsproducts b ON a.idproduct = b.idproduct
-	// 		WHERE b.idcart = :idcart AND dtremoved IS NULL;
-	// 	", [
-	// 		':idcart'=>$this->getidcart()
-	// 	]);
+		$results = $sql->select("
+			SELECT SUM(vlprice) AS vlprice, SUM(vlwidth) AS vlwidth, SUM(vlheight) AS vlheight, SUM(vllength) AS vllength, SUM(vlweight) AS vlweight, COUNT(*) AS nrqtd
+			FROM tb_products a
+			INNER JOIN tb_cartsproducts b ON a.idproduct = b.idproduct
+			WHERE b.idcart = :idcart AND dtremoved IS NULL;
+		", [
+			':idcart'=>$this->getidcart()
+		]);
 
-	// 	if (count($results) > 0) {
-	// 		return $results[0];
-	// 	} else {
-	// 		return [];
-	// 	}
+		if (count($results) > 0) {
+			return $results[0];
+		} else {
+			return [];
+		}
 
-	// }
+	}
 
-	// public function setFreight($nrzipcode)
-	// {
+	public function setFreight($nrzipcode)
+	{
 
-	// 	$nrzipcode = str_replace('-', '', $nrzipcode);
+		$nrzipcode = str_replace('-', '', $nrzipcode);
 
-	// 	$totals = $this->getProductsTotals();
+		$totals = $this->getProductsTotals();
 
-	// 	if ($totals['nrqtd'] > 0) {
+		if ($totals['nrqtd'] > 0) {
 
-	// 		if ($totals['vlheight'] < 2) $totals['vlheight'] = 2;
-	// 		if ($totals['vllength'] < 16) $totals['vllength'] = 16;
+			if ($totals['vlheight'] < 2) $totals['vlheight'] = 2;
+			if ($totals['vllength'] < 16) $totals['vllength'] = 16;
 
-	// 		$qs = http_build_query([
-	// 			'nCdEmpresa'=>'',
-	// 			'sDsSenha'=>'',
-	// 			'nCdServico'=>'40010',
-	// 			'sCepOrigem'=>'09853120',
-	// 			'sCepDestino'=>$nrzipcode,
-	// 			'nVlPeso'=>$totals['vlweight'],
-	// 			'nCdFormato'=>'1',
-	// 			'nVlComprimento'=>$totals['vllength'],
-	// 			'nVlAltura'=>$totals['vlheight'],
-	// 			'nVlLargura'=>$totals['vlwidth'],
-	// 			'nVlDiametro'=>'0',
-	// 			'sCdMaoPropria'=>'S',
-	// 			'nVlValorDeclarado'=>$totals['vlprice'],
-	// 			'sCdAvisoRecebimento'=>'S'
-	// 		]);
+			$qs = http_build_query([
+				'nCdEmpresa'=>'',
+				'sDsSenha'=>'',
+				'nCdServico'=>'40010',
+				'sCepOrigem'=>'09853120',
+				'sCepDestino'=>$nrzipcode,
+				'nVlPeso'=>$totals['vlweight'],
+				'nCdFormato'=>'1',
+				'nVlComprimento'=>$totals['vllength'],
+				'nVlAltura'=>$totals['vlheight'],
+				'nVlLargura'=>$totals['vlwidth'],
+				'nVlDiametro'=>'0',
+				'sCdMaoPropria'=>'S',
+				'nVlValorDeclarado'=>$totals['vlprice'],
+				'sCdAvisoRecebimento'=>'S'
+			]);
 
-	// 		$xml = simplexml_load_file("http://ws.correios.com.br/calculador/CalcPrecoPrazo.asmx/CalcPrecoPrazo?".$qs);
+			$xml = simplexml_load_file("http://ws.correios.com.br/calculador/CalcPrecoPrazo.asmx/CalcPrecoPrazo?".$qs);
 
-	// 		$result = $xml->Servicos->cServico;
+			$result = $xml->Servicos->cServico;
 
-	// 		if ($result->MsgErro != '') {
+			if ($result->MsgErro != '') {
 
-	// 			Cart::setMsgError($result->MsgErro);
+				Cart::setMsgError($result->MsgErro);
 
-	// 		} else {
+			} else {
 
-	// 			Cart::clearMsgError();
+				Cart::clearMsgError();
 
-	// 		}
+			}
 
-	// 		$this->setnrdays($result->PrazoEntrega);
-	// 		$this->setvlfreight(Cart::formatValueToDecimal($result->Valor));
-	// 		$this->setdeszipcode($nrzipcode);
+			$this->setnrdays($result->PrazoEntrega);
+			$this->setvlfreight(Cart::formatValueToDecimal($result->Valor));
+			$this->setdeszipcode($nrzipcode);
 
-	// 		$this->save();
+			$this->save();
 
-	// 		return $result;
+			return $result;
 
-	// 	} else {
+		} else {
 
 
 
-	// 	}
+		}
 
-	// }
+	}
 
-	// public static function formatValueToDecimal($value):float
-	// {
+	public static function formatValueToDecimal($value):float
+	{
 
-	// 	$value = str_replace('.', '', $value);
-	// 	return str_replace(',', '.', $value);
+		$value = str_replace('.', '', $value);
+		return str_replace(',', '.', $value);
 
-	// }
+	}
 
-	// public static function setMsgError($msg)
-	// {
+	public static function setMsgError($msg)
+	{
 
-	// 	$_SESSION[Cart::SESSION_ERROR] = $msg;
+		$_SESSION[Cart::SESSION_ERROR] = $msg;
 
-	// }
+	}
 
 	public static function getMsgError()
 	{
@@ -286,37 +286,37 @@ class Cart extends Model {
 
 	}
 
-	// public function updateFreight()
-	// {
+	public function updateFreight()
+	{
 
-	// 	if ($this->getdeszipcode() != '') {
+		if ($this->getdeszipcode() != '') {
 
-	// 		$this->setFreight($this->getdeszipcode());
+			$this->setFreight($this->getdeszipcode());
 
-	// 	}
+		}
 
-	// }
+	}
 
-	// public function getValues()
-	// {
+	public function getValues()
+	{
 
-	// 	$this->getCalculateTotal();
+		$this->getCalculateTotal();
 
-	// 	return parent::getValues();
+		return parent::getValues();
 
-	// }
+	}
 
-	// public function getCalculateTotal()
-	// {
+	public function getCalculateTotal()
+	{
 
-	// 	$this->updateFreight();
+		$this->updateFreight();
 
-	// 	$totals = $this->getProductsTotals();
+		$totals = $this->getProductsTotals();
 
-	// 	$this->setvlsubtotal($totals['vlprice']);
-	// 	$this->setvltotal($totals['vlprice'] + (float)$this->getvlfreight());
+		$this->setvlsubtotal($totals['vlprice']);
+		$this->setvltotal($totals['vlprice'] + (float)$this->getvlfreight());
 
-	// }
+	}
 
 }
 
